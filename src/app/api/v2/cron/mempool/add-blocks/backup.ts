b121a43bdd4db258e7cae56cf894a058755c7f96
crypticmeta <crypticmetadev@gmail.com>
2023-11-21 23:22:49 +0530

    updated mempool cron

--- src/app/api/v2/cron/mempool/add-blocks/backup.ts
+++ src/app/api/v2/cron/mempool/add-blocks/backup.ts
@@ -42,11 +42,11 @@ async function upsertBlockData(latestBlockhash: string) {
 }
 
 // Function to verify the previous blockhash
-async function verifyPreviousBlockhash(blockhash: string): Promise<boolean> {
+async function verifyPreviousBlockhash(blockhash: string): Promise<void> {
   const currentBlockDetail = await Block.findOne({ id: blockhash });
   if (!currentBlockDetail.previousblockhash) {
     console.warn("No previous block hash found in db.");
-    return false;
+    return;
   }
   const previousBlockhash = currentBlockDetail.previousblockhash;
 
@@ -56,7 +56,7 @@ async function verifyPreviousBlockhash(blockhash: string): Promise<boolean> {
     console.warn(
       `No block details found for previous block hash ${previousBlockhash}`
     );
-    return false;
+    return;
   }
 
   const txCount = await Tx.countDocuments({
@@ -64,93 +64,42 @@ async function verifyPreviousBlockhash(blockhash: string): Promise<boolean> {
   });
 
   if (txCount !== previousBlockDetail.tx_count) {
-    console.log({
-      inDb: txCount,
-      total: previousBlockDetail.tx_count,
-      previousBlockhash,
-    });
+    console.log({ inDb: txCount, total: previousBlockDetail.tx_count });
     console.log("Transaction count mismatch, updating...");
     await addBlockTxToDB(previousBlockhash);
-    return true;
   } else {
     console.log("Transaction count verified, no need for update.");
-    return false;
   }
 }
 
 // Add Transactions to Database
 
-const fetchTransactions = async (index: number, blockhash: string) => {
-  try {
-    console.log({ index, blockhash });
-    const response = await axios.get(
-      `https://mempool.space/api/block/${blockhash}/txs/${index}`
-    );
-    return response.data;
-  } catch (error: any) {
-    if (error.response && error.response.status === 404) {
-      return []; // Return empty array for 404 errors
-    } else {
-      throw error; // Re-throw other errors
-    }
-  }
-};
-
 async function addBlockTxToDB(blockhash: string) {
-  await dbConnect();
   const dbBlockDetail = await Block.findOne({ id: blockhash });
   console.log(dbBlockDetail, "DB To process");
   const bulkOps: Array<any> = [];
   const newTxIds: string[] = [];
   const inscriptionTxIds: { txid: string; inscriptions: any }[] = [];
 
-  let existingTxIds = [];
-  let existingTxIdSet = null;
-  if (dbBlockDetail) {
-    existingTxIds = await Tx.find(
-      { blockhash: new ObjectId(dbBlockDetail._id) },
-      "txid"
-    );
-    existingTxIdSet = new Set(existingTxIds.map((tx) => tx.txid));
-  }
+  const blockTxsResponse = await axios.get(
+    `https://mempool.space/api/block/${blockhash}/txs`
+  );
+  const blockTxsData: MempoolBlockTx[] = blockTxsResponse.data;
 
-  if (dbBlockDetail.tx_count === existingTxIds.length) {
-    dbBlockDetail.processed = true;
-    dbBlockDetail.save();
-    return { newTxIds, inscriptionTxIds };
-  }
+  const existingTxIds = await Tx.find(
+    { blockhash: new ObjectId(dbBlockDetail._id) },
+    "txid"
+  );
 
-  let startIndex = existingTxIds.length;
-  let allBlockTxsData: any = [];
-  const maxTransactions = 100;
-  const batchSize = 25; // Each call handles 25 transactions
-  const parallelCalls = 10; // Total 4 API calls
-  const promises = [];
-  for (let i = 0; i < parallelCalls; i++) {
-    promises.push(fetchTransactions(startIndex, blockhash));
-    startIndex += batchSize;
-  }
-
-  // console.log({ promises });
-
-  const results = await Promise.all(promises);
-  for (const blockTxsData of results) {
-    if (blockTxsData.length === 0) {
-      startIndex = maxTransactions; // Break out of the loop
-      break;
-    }
-    allBlockTxsData = allBlockTxsData.concat(blockTxsData);
-  }
-
-  // console.log({ results }, "RES");
+  const existingTxIdSet = new Set(existingTxIds.map((tx) => tx.txid));
 
   console.log({
-    txsSentByMempool: allBlockTxsData.length,
+    txsSentByMempool: blockTxsData.length,
     txMempoolShouldBe: dbBlockDetail.tx_count,
     existingTxInDB: existingTxIds.length,
   });
 
-  for (const item of allBlockTxsData) {
+  for (const item of blockTxsData) {
     const {
       vin: inputs,
       vout: outputs,
@@ -162,7 +111,7 @@ async function addBlockTxToDB(blockhash: string) {
       status,
     } = item;
 
-    if (existingTxIdSet && existingTxIdSet.has(txid)) continue;
+    if (existingTxIdSet.has(txid)) continue;
 
     newTxIds.push(txid);
 
@@ -176,7 +125,7 @@ async function addBlockTxToDB(blockhash: string) {
           size,
           weight,
           fee,
-          status: "confirmed",
+          status,
           vin: inputs,
           vout: outputs,
           height: dbBlockDetail.height,
@@ -231,32 +180,10 @@ export async function GET(req: NextRequest, res: NextResponse) {
 
     try {
       latestBlockHeight = await getLatestBlockHeight();
-      let lastSavedBlock = await Block.findOne({ processed: false }).sort({
-        height: -1,
-      });
-
-      if (!lastSavedBlock) {
-        lastSavedBlock = await Block.findOne({}).sort({
-          height: -1,
-        });
-      }
+      const lastSavedBlock = await Block.findOne().sort({ height: -1 });
       lastSavedBlockHeight = lastSavedBlock
-        ? lastSavedBlock.processed
-          ? lastSavedBlock.height + 1
-          : lastSavedBlock.height
+        ? lastSavedBlock.height
         : latestBlockHeight;
-
-      console.log({ lastSavedBlockHeight });
-
-      // If the latest block height is the same as the last saved block height, process it
-      if (lastSavedBlockHeight) {
-        console.log("Processing block at height: ", lastSavedBlockHeight);
-        await processBlockHeight(lastSavedBlockHeight);
-      } else {
-        console.log(
-          "No new blocks to process. Latest block height is already saved."
-        );
-      }
     } catch (e) {
       console.error("Failed to get block information:", e);
       return NextResponse.json(
@@ -264,31 +191,60 @@ export async function GET(req: NextRequest, res: NextResponse) {
         { status: 500 }
       );
     }
+
+    if (latestBlockHeight >= lastSavedBlockHeight) {
+      for (
+        let currentHeight =
+          latestBlockHeight === lastSavedBlockHeight
+            ? latestBlockHeight
+            : lastSavedBlockHeight + 1;
+        currentHeight <= latestBlockHeight;
+        currentHeight += 1
+      ) {
+        console.log("processing height: ", currentHeight);
+        if (
+          currentHeight > lastSavedBlockHeight + 1 ||
+          currentHeight > latestBlockHeight
+        ) {
+          return NextResponse.json({
+            message: `Sync completed from height ${lastSavedBlockHeight} to ${currentHeight}`,
+          });
+        }
+
+        const currentBlockHash = await fetchBlockhashFromHeight(currentHeight);
+        const operationMessage = await upsertBlockData(currentBlockHash);
+
+        // Verify the previous blockhash
+
+        if (latestBlockHeight !== lastSavedBlockHeight)
+          await verifyPreviousBlockhash(currentBlockHash);
+
+        // Add transactions for the current block to the database
+        await addBlockTxToDB(currentBlockHash);
+
+        // Fetch the previous block hash. This could be based on your database or an external service
+        const previousBlockHash = await getPreviousBlockhash(currentBlockHash); // Implement this function
+        if (!previousBlockHash) {
+          return NextResponse.json({
+            message: `Sync completed from height ${lastSavedBlockHeight} to ${currentHeight}`,
+          });
+          break; // Exit loop if no previous block
+        }
+      }
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.json(
       { message: "Error fetching and saving txids" },
       { status: 500 }
     );
+  } finally {
+    return NextResponse.json({
+      message: "Processing completed, check logs for details",
+    });
   }
 }
 
-async function processBlockHeight(blockHeight: number) {
-  const currentBlockHash = await fetchBlockhashFromHeight(blockHeight);
-  const operationMessage = await upsertBlockData(currentBlockHash);
-
-  // Verify the previous blockhash
-  // await verifyPreviousBlockhash(currentBlockHash);
-
-  // Add transactions for the current block to the database
-  await addBlockTxToDB(currentBlockHash);
-
-  // Fetch the previous block hash. This could be based on your database or an external service
-  // const previousBlockHash = await getPreviousBlockhash(currentBlockHash);
-  // if (!previousBlockHash) {
-  //   console.log(`Sync completed for block height ${blockHeight}`);
-  // }
-}
 // New GET function for a single block height
 export async function POST(req: NextRequest, res: NextResponse) {
   await dbConnect();
@@ -310,13 +266,10 @@ export async function POST(req: NextRequest, res: NextResponse) {
     const operationMessage = await upsertBlockData(blockHash);
 
     // Verify the previous blockhash
-    const updatingPrevious = await verifyPreviousBlockhash(blockHash);
+    await verifyPreviousBlockhash(blockHash);
 
     // Add transactions for this block to the database
-    if (!updatingPrevious) await addBlockTxToDB(blockHash);
-    else {
-      console.log("not adding new blockhash items as older one was updated");
-    }
+    await addBlockTxToDB(blockHash);
 
     return NextResponse.json({
       message: `Sync completed for block height ${blockHeight}`,
