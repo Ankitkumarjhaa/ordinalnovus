96f233aa08184a55f4481c767fcbef623c6e393f
crypticmeta <crypticmetadev@gmail.com>
2023-12-04 19:37:56 +0530

    modified dockerfile and github workflow

--- src/lib/mixpanelConfig.ts
+++ src/lib/mixpanelConfig.ts
@@ -7,26 +7,17 @@ const initMixpanel = () => {
     return;
   }
 
-  let mixpanelToken =
-    process.env.NEXT_PUBLIC_MIXPANEL || "670cc838c8dd3bbaff43b48867c465c0"; // Default token
+  let mixpanelToken = "670cc838c8dd3bbaff43b48867c465c0"; // Default token
 
-  console.log(process.env.NEXT_PUBLIC_URL, "URL");
-  if (process.env.NEXT_PUBLIC_URL !== "https://ordinalnovus.com") {
-    mixpanelToken = "670cc838c8dd3bbaff43b48867c465c0";
+  if (process.env.NEXT_PUBLIC_URL === "https://ordinalnovus.com") {
+    mixpanelToken = "863cacfbbada1077c3128e23849ff611";
   }
 
-  console.log(mixpanelToken, "TOKEN");
   mixpanel.init(mixpanelToken, {
-    debug: true || process.env.NODE_ENV === "production",
-  });
-  mixpanel.set_config({
-    ip: true,
-    ignore_dnt: true,
+    debug: true || process.env.NODE_ENV !== "production",
   });
 
   isMixpanelInitialized = true;
 };
 
 export default initMixpanel;
-
-export const dynamic = "force-dynamic";
