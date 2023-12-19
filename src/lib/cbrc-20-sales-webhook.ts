9c65493d57d72ea6ecfba361546b701b58cefb3a
crypticmeta <crypticmetadev@gmail.com>
2023-12-19 18:05:17 +0530

    added new sales hook

--- src/lib/cbrc-20-sales-webhook.ts
+++ src/lib/cbrc-20-sales-webhook.ts
@@ -1,4 +1,5 @@
 export const webhooks = [
   "https://discord.com/api/webhooks/1186060583754272879/YhL-hewnAl4fsCTICWWnh4JdjTq8fwTIAqb6HYcaQvn-Sjhe8AoJhJ-uU--68X3nP7Ba",
   "https://discord.com/api/webhooks/1186094130674270218/WUyfIDOBLtTUGRMpvGyWjZHlmfT4s-XVwpbORhNpT_AmU5V83KZQIG2iIIlXHSC6NbGS",
+  "https://discord.com/api/webhooks/1186477639926562886/OBSBoOAH1Rtd1REvP4Y3jsvOVg9lPfI5HeA9DlQBJwjMOnQZ1bj7v61Avsx_qXnLIyeK",
 ];
