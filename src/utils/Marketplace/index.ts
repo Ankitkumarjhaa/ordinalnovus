07b61291d2425cfc2c76398472e335803c483224
crypticmeta <crypticmetadev@gmail.com>
2023-12-24 10:17:17 +0530

    dummy check

--- src/utils/Marketplace/index.ts
+++ src/utils/Marketplace/index.ts
@@ -105,13 +105,35 @@ const recommendedFeeRate = async (fee_rate?: FeeRateTier) =>
     .then((response) => response.json())
     .then((data) => data[fee_rate || feeLevel]);
 
-async function doesUtxoContainInscription(utxo: { txid: any; vout: any }) {
-  const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_PROVIDER}/api/output/${utxo.txid}:${utxo.vout}`
-  );
+async function doesUtxoContainInscription(
+  utxo: AddressTxsUtxo
+): Promise<boolean> {
+  const apiUrl = process.env.NEXT_PUBLIC_PROVIDER;
+  if (!apiUrl) {
+    // If the API URL is not set, return true as per your requirement
+    console.warn("API provider URL is not defined in environment variables");
+    return true;
+  }
 
-  return data.inscriptions.length > 0;
+  try {
+    const response = await axios.get(
+      `${apiUrl}/api/output/${utxo.txid}:${utxo.vout}`
+    );
+
+    if (response.data && Array.isArray(response.data.inscriptions)) {
+      return response.data.inscriptions.length > 0;
+    } else {
+      // If the data is not in the expected format, return true
+      console.warn("Invalid data structure received from API");
+      return true;
+    }
+  } catch (error) {
+    // In case of any API error, return true
+    console.error("Error in doesUtxoContainInscription:", error);
+    return true;
+  }
 }
+
 async function mapUtxos(utxosFromMempool: AddressTxsUtxo[]): Promise<UTXO[]> {
   const ret: UTXO[] = [];
   for (const utxoFromMempool of utxosFromMempool) {
