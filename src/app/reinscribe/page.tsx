aae87cb2c3d652809582ab21c2cdcc534e58299d
crypticmeta <crypticmetadev@gmail.com>
2024-01-12 00:11:24 +0530

    hide reinscribe page

--- src/app/reinscribe/page.tsx
+++ src/app/reinscribe/page.tsx
@@ -2,7 +2,8 @@ import Crafter from "@/Views/Crafter";
 import React from "react";
 
 function page() {
-  return <Crafter mode="reinscribe" />;
+  return <></>;
+  // return <Crafter mode="reinscribe" />;
 }
 
 export default page;
